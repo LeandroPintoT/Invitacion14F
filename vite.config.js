f8035ab78e3fecd9d3b63b833e7a38a23fee081d
LeandroPintoT <leandro.pinto.t@mail.pucv.cl>
2024-02-04 23:11:49 -0300

    fix: quita cambios en directorio dist.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Invitacion14F/dist/",
+  base: "/invitacion14f/",
   plugins: [
     vue(),
   ],
