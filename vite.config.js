a777b64fb60c6a84cbd799e85cdd740cfa6ba809
LeandroPintoT <leandro.pinto.t@mail.pucv.cl>
2024-02-04 22:57:51 -0300

    chg: intento de fix para deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Invitacion14F/",
+  base: "/Invitacion14F/dist/",
   plugins: [
     vue(),
   ],
